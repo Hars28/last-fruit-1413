7637aa2c2b5e3b59ce8153f33dbcd35e4047ab6a
Hars28 <harsh.sing9055@gmail.com>
2022-08-25 23:19:05 +0530

    Done home page without carousel

--- best-buy/src/App.js
+++ best-buy/src/App.js
@@ -2,10 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
+import { Home_carosul } from './Components/Home_carosul/Home_carosul';
+import Home from './Pages/Home';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
       <Footer/>
     </div>
   );
